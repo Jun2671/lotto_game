bd32cc0748850e2e194017092c414117282d9379
hojun176 <alkdoi12@naver.com>
2024-10-30 18:13:06 +0900

    feat: 발행한 로또의 번호 출력

--- lotto_game.js
+++ lotto_game.js
@@ -1,3 +1,5 @@
+//Math : 자바스크립트에서 랜덤함수 생성
+//Array.form() : 객체를 배열로 바꾸어 주는 함수
 const readline = require("readline");
 
 // readline 인터페이스 설정
@@ -6,29 +8,31 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-function isStart() {}
+function lottoRandom(price) {
+  let count = price/1000; //로또 갯수 생성
+  let newArray = [];
+  for(let i = 0 ; i< count ; i++) {
+    let set = new Set();
+    // while(set.size<6) {
+    //   set.add(Math.floor(Math.random()*45)+1);
+    // }
+    for(let j =0; j<6; j++){
+      set.add(Math.floor(Math.random()*45)+1);
+    }
+    newArray.push(Array.from(set));
+  }
+  console.log(newArray);
+  return newArray;
+}
 
 // readline을 사용하여 입력받기
 rl.question("로또를 얼마치 구매하셨습니까? ", (price) => {
-  console.log(`${price / 1000}개를 구매하셨습니다`);
-  let conprice = price /1000;
-  
-//랜덤 숫자 6개 출력 이중 for문 사용
-for(i = 0; i < conprice; i++){
-  let Readom = [];
-  for(j=0; j<6; j++){
-    
-    Readom.push(Math.floor(Math.random()*45)+1);    //45개의 랜덤 숫자 생성   Math: 랜덤함수
-    
-  }
-  console.log(Readom);
-}
-
+  lottoRandom(price);
   rl.question("당첨번호를 입력하세요(,형태로 적어주세요) ", (number) => {
     console.log(`당첨번호는 ${number}입니다`);
     rl.question("보너스 번호를 입력하세요", (bonus) => {
       console.log(`보너스번호는 ${bonus} 입니다`);
     });
-    rl.close(); // 입력 종료
   });
+  rl.close(); // 입력 종료
 });
